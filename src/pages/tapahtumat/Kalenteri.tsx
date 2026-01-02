1f8b1b24950dfd573d9a61ace64cbb33e150a6b2
VP-MaxHax <vp.juselius@gmail.com>
2026-01-02 17:27:03 +0200

    Added Fullcalendar library to the project and did some testing and fitting to website

--- src/pages/tapahtumat/Kalenteri.tsx
+++ src/pages/tapahtumat/Kalenteri.tsx
@@ -1,8 +1,48 @@
-export default function About() {
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import fiLocale from '@fullcalendar/core/locales/fi'
+import './Kalenteri.css'
+
+export default function Calendar() {
   return (
-    <div id="about">
-      <h2>Tapahtumat</h2>
-      <p>Kalenterin tapahtumat löytyvät täältä.</p>
+    <div id="calendar">
+      <h1>Tapahtumakalenteri</h1>
+      <FullCalendar
+        plugins={[ dayGridPlugin ]}
+        locale={fiLocale}
+        initialView="dayGridMonth"
+        editable={false}
+        selectable={true}
+        contentHeight={'60%'}
+        events={[{
+          id: '1',
+          title: 'Syyskokous',
+          start: '2026-01-04',
+          url: './events/syyskokous'
+        },
+        {
+          id: '2',
+          title: 'ATK-***',
+          start: '2026-01-05',
+          end: '2026-01-08',
+          url: './events/atk-***'
+        },
+        {
+          id: '3',
+          groupId: '999',
+          title: 'BGW',
+          start: '2026-01-08T16:00:00',
+          url: './events/bgw'
+        },
+        {
+          id: '4',
+          groupId: '999',
+          title: 'BGW',
+          start: '2026-01-15T16:00:00',
+          url: './events/bgw'
+        }
+        ]}
+      />
     </div>
   )
 }
